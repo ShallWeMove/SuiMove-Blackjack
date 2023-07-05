e1d8be0730fb5070a5ed5726d3f0d3e573d81621
Desmos <42434324+ckdals3121@users.noreply.github.com>
2023-07-05 13:17:34 +0900

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import WebSocket from 'ws';
 import dotenv from "dotenv";
-import { getProvider, getSigner, startGame, shuffle, endGame, getCard, test  } from './moveCall';
+import { getProvider, getSigner, startGame, gameReady, endGame, getCard } from './moveCall';
 import { encrypt, decrypt } from './encrypt';
 
 dotenv.config();
@@ -26,37 +26,11 @@ wss.on('connection', (ws: WebSocket) => {
             const result = startGame(signer)
             // return flag: 'game start done'
         } 
-        
-        else if (flag == 'player card open') {
-            const data = {
-                flag: 'player card open',
-                args: 'Card Object'
-            };
-            ws.send(JSON.stringify(data))
-        }
 
-        else if (flag == 'shuffle') {
+        else if (flag == 'game ready') {
             // card shuffle move call
-            const result = shuffle(signer)
+            const result = gameReady(signer)
             // return flag: 'shuffle done'
-        }
-
-        else if (flag == 'card open') {
-            // player 1 card open
-            const playerFirst = getCard(signer, false)
-            // return flag: 'playerFirst done'
-            
-            // dealer 1 card open
-            const dealerFirst = getCard(signer, false)
-            // return flag: 'dealerFirst done'
-
-            // player 1 card open
-            const playerSecond = getCard(signer, false)
-            // return flag: 'playerSecond done'
-
-            // dealer 1 card hide
-            const dealerSecond = getCard(signer, true)
-            // return flag: 'dealerSecond done'
         }
 
         else if (flag == 'Go') {
@@ -69,15 +43,6 @@ wss.on('connection', (ws: WebSocket) => {
         else if (flag == 'Stop') {
             const result = endGame(signer)
             console.log("Stop");
-            // dealer cards open (if necessary)
-            // return opened card objects (maybe list)
-
-            // check who is winner
-            // return result
-            // if player win, transfer sui to player, 
-            // if player lose, transfer sui to dealer
-
-            // return flag: 'Stop done' // not yet
         }
         
 
