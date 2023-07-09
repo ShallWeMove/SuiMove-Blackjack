d59dca203fabf5d69672cf7833521b3b15161f58
TAEWON PARK <taewonpark@TAEWONui-MacBookPro.local>
2023-07-09 12:06:42 +0900

    update get game information and solve the error

---
+++ frontend/src/components/GameTableInfo.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import { Typography, Box, TextField, Button } from "@mui/material";
+
+const GameTableInfo = ({
+    gameTableData, 
+    cardDeckData, 
+    dealerHandData, 
+    playerHandData, 
+}) => {
+
+    return (
+        <Box>
+            <Typography>dealer hand : {gameTableData.dealer_hand} total card : {dealerHandData.total_cards_number}</Typography>
+            <Typography>dealer cards</Typography>
+            {dealerHandData.cards.map((card)=>(<Typography>{card}</Typography>))}
+
+            <Typography>player hand : {gameTableData.player_hand} total card : {playerHandData.total_cards_number}</Typography>
+            <Typography>player cards</Typography>
+            {playerHandData.cards.map((card)=>(<Typography>{card}</Typography>))}
+
+            <Typography>card deck : {gameTableData.card_deck} total card : {cardDeckData.total_cards_number}</Typography>
+            <Typography>card deck cards</Typography>
+            {cardDeckData.cards.map((card)=>(<Typography>{card}</Typography>))}
+        </Box>
+    )
+}
+
+export default GameTableInfo;
