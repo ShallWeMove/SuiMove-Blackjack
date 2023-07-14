12f865c8a66f001ade93d35e5ca59cf794d1f482
harrier999 <harrier999@naver.com>
2023-07-14 14:25:57 +0900

    [ADD] SideBar Component

---
+++ frontend/src/components/SideBar.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function SideBar() {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const ws = new WebSocket('ws://localhost:8765');
+
+
+    ws.onmessage = (event) => {
+      toast(event.data, { autoClose: 10000 });
+    };
+
+    ws.onclose = () => {
+      console.log('WebSocket connection closed');
+    };
+
+    setSocket(ws);
+
+    return () => {
+      ws.close();
+    };
+  }, []);
+
+  return (
+    <ToastContainer 
+      position="bottom-right" 
+      newestOnTop
+      pauseOnFocusLoss={false}
+    />
+  );
+}
