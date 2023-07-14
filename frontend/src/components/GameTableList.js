d70d07a8c3b04607576b09c7e7730da7db54a316
JOEYLEE1121 <joeylee1121@gmail.com>
2023-07-14 19:50:19 +0900

    add betting amount page (without amount checking)

--- frontend/src/components/GameTableList.js
+++ frontend/src/components/GameTableList.js
@@ -5,10 +5,10 @@ import { useEffect, useState } from "react";
 
 
 const GameTableList = ({
-     allGameTables,
-     setGameTableObjectId,
-     handleGoToGameButtonClick,
-     }) => {      
+    allGameTables,
+    setGameTableObjectId,
+    handleGoToGameButtonClick,
+}) => {
 
     const [opens, setOpens] = useState([]);
 
@@ -18,78 +18,78 @@ const GameTableList = ({
 
     return allGameTables && (
         <Box
-        sx={{
-            marginX: "auto",
-            width: "50%",
-        }}>
-            <Typography
-            variant="h3"
             sx={{
-                textAlign: "center",
-                margin: "20px auto",
-                fontSize: "30px",
-                fontWeight: "800",
-            }}
+                marginX: "auto",
+                width: "50%",
+            }}>
+            <Typography
+                variant="h3"
+                sx={{
+                    textAlign: "center",
+                    margin: "20px auto",
+                    fontSize: "30px",
+                    fontWeight: "800",
+                }}
             >Game Table List</Typography>
             <Grid container rowSpacing={3} columnSpacing={1}>
-            {allGameTables.map((t, i) => (
-                     <Grid key={i} item xs={4}>
-                        
-                        <Box 
-                        onClick={() => {
-                            if (t.data.content.fields.is_playing < 1)  {
+                {allGameTables.map((t, i) => (
+                    <Grid key={i} item xs={4}>
+
+                        <Box
+                            onClick={() => {
+                                // if (t.data.content.fields.is_playing < 1)  {
                                 setTimeout(() => {
                                     setGameTableObjectId(t.data.objectId);
                                     handleGoToGameButtonClick(t.data.objectId);
                                 }, 1500);
                                 setOpens(opens.map((o, j) => i === j ? true : o));
-                            }
-                        }}
-                        sx={{
-                            width: '160px',
-                            height: '240px',
-                            position: 'relative',
-                            marginX: 'auto',
-                            transformStyle: 'preserve-3d',
-                            transformOrigin: 'center',
-                            transition: 'transform .5s',
-                            transform: `${opens[i] ? `rotateY(180deg) scale(1.1)` : `rotateY(0deg)`}`,
-                            "&:hover": {
-                                // transform: `rotateY(180deg) scale(1.1)`,
-                                transform: `${t.data.content.fields.is_playing >= 1 ? '' : 'scale(1.1)'}`,
-                            }
-                        }}>
+                                // }
+                            }}
+                            sx={{
+                                width: '160px',
+                                height: '240px',
+                                position: 'relative',
+                                marginX: 'auto',
+                                transformStyle: 'preserve-3d',
+                                transformOrigin: 'center',
+                                transition: 'transform .5s',
+                                transform: `${opens[i] ? `rotateY(180deg) scale(1.1)` : `rotateY(0deg)`}`,
+                                "&:hover": {
+                                    // transform: `rotateY(180deg) scale(1.1)`,
+                                    transform: `${t.data.content.fields.is_playing >= 1 ? '' : 'scale(1.1)'}`,
+                                }
+                            }}>
                             {t.data.content.fields.is_playing >= 1 && (
-                                <Typography 
-                                color='primary'
-                                sx={{
-                                    zIndex: '15',
-                                    position: 'absolute',
-                                    left: '10px',
-                                    top: '10px',
-                                    fontSize: '20px',
-                                    fontWeight: '700',
-                                    opacity: '0.8',
-                                }}>Playing</Typography>
-                            )}
-                            <Box 
+                                <Typography
+                                    color='primary'
                                     sx={{
-                                        zIndex: '10',
-                                        width: '100%',
-                                        height: '100%',
+                                        zIndex: '15',
                                         position: 'absolute',
-                                        left: '0',
-                                        top: '0',
-                                        backgroundImage: `url(${card})`,
-                                        backgroundSize: 'cover',
-                                        backgroundPosition: 'center',
-                                        cursor: `${t.data.content.fields.is_playing < 1 && 'pointer'}`,
-                                        backfaceVisibility: 'hidden',
-                                        opacity: `${t.data.content.fields.is_playing >= 1 ? '0.3' : '1'}`,
-                                    }}
-                                />
+                                        left: '10px',
+                                        top: '10px',
+                                        fontSize: '20px',
+                                        fontWeight: '700',
+                                        opacity: '0.8',
+                                    }}>Playing</Typography>
+                            )}
+                            <Box
+                                sx={{
+                                    zIndex: '10',
+                                    width: '100%',
+                                    height: '100%',
+                                    position: 'absolute',
+                                    left: '0',
+                                    top: '0',
+                                    backgroundImage: `url(${card})`,
+                                    backgroundSize: 'cover',
+                                    backgroundPosition: 'center',
+                                    cursor: `${t.data.content.fields.is_playing < 1 && 'pointer'}`,
+                                    backfaceVisibility: 'hidden',
+                                    opacity: `${t.data.content.fields.is_playing >= 1 ? '0.3' : '1'}`,
+                                }}
+                            />
 
-                                <Box 
+                            <Box
                                 sx={{
                                     zIndex: '5',
                                     width: '100%',
@@ -106,17 +106,17 @@ const GameTableList = ({
                                     backfaceVisibility: 'hidden',
                                     opacity: `${t.data.content.fields.is_playing >= 1 ? '0.3' : '1'}`,
                                 }}
-                                />
+                            />
 
                         </Box>
 
-                      
-                    </Grid>
-            ))}
 
-        </Grid>
+                    </Grid>
+                ))}
+
+            </Grid>
         </Box>
-        
+
     )
 }
 
