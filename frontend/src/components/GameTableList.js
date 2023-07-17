7423d10df45dd256926c042e4142cbd9f8df7eaf
JOEYLEE1121 <joeylee1121@gmail.com>
2023-07-17 11:40:14 +0900

    [Feat] Added alert

--- frontend/src/components/GameTableList.js
+++ frontend/src/components/GameTableList.js
@@ -3,6 +3,7 @@ import config from "../config.json";
 import card from "../images/cards/card.png";
 import { useEffect, useState } from "react";
 import { useWallet } from "@suiet/wallet-kit";
+import { Snackbar, Alert } from "@mui/material";
 
 
 const GameTableList = ({
@@ -13,6 +14,7 @@ const GameTableList = ({
 }) => {
     const wallet = useWallet();
     const [gameTableOpens, setGameTableOpens] = useState([]);
+    const [openSnackbar, setOpenSnackbar] = useState(false);
     const game_status = ["Available Game", "Ready", "Playing", "Game End"]
 
     useEffect(() => {
@@ -35,46 +37,47 @@ const GameTableList = ({
                 }}
             >Game Table List</Typography>
             <Grid container rowSpacing={3} columnSpacing={1}>
-            {allGameTables.map((table, i) => (
-                     <Grid key={i} item xs={4}>
-                        
-                        <Box 
-                        onClick={() => {
-                            if ((table.data.content.fields.is_playing < 1) || (table.data.content.fields.player_address === wallet.address)) {
-                                console.log("you can play the game")
-                                setLoading(true);
-                                setGameTableObjectId(table.data.objectId);
-                                handlGameTableButtonClick(table.data.objectId);
-                                setGameTableOpens(gameTableOpens.map((open, j) => i === j ? true : open));
-                            } else {
-                                console.log("you can't play the game")
-                            }
-                        }}
-                        sx={{
-                            width: '160px',
-                            height: '240px',
-                            position: 'relative',
-                            marginX: 'auto',
-                            transformStyle: 'preserve-3d',
-                            transformOrigin: 'center',
-                            transition: 'transform .5s',
-                            transform: `${gameTableOpens[i] ? `rotateY(180deg) scale(1.1)` : `rotateY(0deg)`}`,
-                            "&:hover": {
-                                // transform: `rotateY(180deg) scale(1.1)`,
-                                // transform: `${t.data.content.fields.is_playing >= 1 ? '' : 'scale(1.1)'}`,
-                                transform: `${gameTableOpens[i] ? `rotateY(180deg) scale(1.1)` : `rotateY(0deg) scale(1.1)`}`,
-                            }
-                        }}>
-                                <Typography
-                                    sx={{
-                                        zIndex: '15',
-                                        position: 'absolute',
-                                        left: '10px',
-                                        top: '10px',
-                                        fontSize: '20px',
-                                        fontWeight: '700',
-                                        opacity: '0.8',
-                                    }}>{game_status[parseInt(table.data.content.fields.is_playing)]}</Typography>
+                {allGameTables.map((table, i) => (
+                    <Grid key={i} item xs={4}>
+
+                        <Box
+                            onClick={() => {
+                                if ((table.data.content.fields.is_playing < 1) || (table.data.content.fields.player_address === wallet.address)) {
+                                    console.log("you can play the game")
+                                    setLoading(true);
+                                    setGameTableObjectId(table.data.objectId);
+                                    handlGameTableButtonClick(table.data.objectId);
+                                    setGameTableOpens(gameTableOpens.map((open, j) => i === j ? true : open));
+                                } else {
+                                    setOpenSnackbar(true);
+                                    console.log("you can't play the game")
+                                }
+                            }}
+                            sx={{
+                                width: '160px',
+                                height: '240px',
+                                position: 'relative',
+                                marginX: 'auto',
+                                transformStyle: 'preserve-3d',
+                                transformOrigin: 'center',
+                                transition: 'transform .5s',
+                                transform: `${gameTableOpens[i] ? `rotateY(180deg) scale(1.1)` : `rotateY(0deg)`}`,
+                                "&:hover": {
+                                    // transform: `rotateY(180deg) scale(1.1)`,
+                                    // transform: `${t.data.content.fields.is_playing >= 1 ? '' : 'scale(1.1)'}`,
+                                    transform: `${gameTableOpens[i] ? `rotateY(180deg) scale(1.1)` : `rotateY(0deg) scale(1.1)`}`,
+                                }
+                            }}>
+                            <Typography
+                                sx={{
+                                    zIndex: '15',
+                                    position: 'absolute',
+                                    left: '10px',
+                                    top: '10px',
+                                    fontSize: '20px',
+                                    fontWeight: '700',
+                                    opacity: '0.8',
+                                }}>{game_status[parseInt(table.data.content.fields.is_playing)]}</Typography>
                             <Box
                                 sx={{
                                     zIndex: '10',
@@ -114,12 +117,20 @@ const GameTableList = ({
                             />
 
                         </Box>
-
-
                     </Grid>
                 ))}
 
             </Grid>
+            <Snackbar
+                open={openSnackbar}
+                autoHideDuration={3000}
+                onClose={() => setOpenSnackbar(false)}
+                anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+            >
+                <Alert onClose={() => setOpenSnackbar(false)} severity="warning" sx={{ width: '100%' }}>
+                    You can't play the game.
+                </Alert>
+            </Snackbar>
         </Box>
 
     )
