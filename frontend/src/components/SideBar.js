284674b578549b29022c172a51d86838f51e51e7
perth-taewon <ptw00317@naver.com>
2023-07-18 14:40:51 +0000

    update websoket

--- frontend/src/components/SideBar.js
+++ frontend/src/components/SideBar.js
@@ -3,7 +3,8 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function SideBar() {
-  const [socket, setSocket] = useState(null);
+  //const [socket, setSocket] = useState(null);
+  const ws = new WebSocket('wss://35.200.66.115:8080');
 
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:8080');
